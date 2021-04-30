57091d4ce7cc1a83fc5c5b0940a1c60b6e2ac32e
mrlandfried <30705227+mrlandfried@users.noreply.github.com>
2021-04-30 07:30:24 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    
+    //1,2,3,4,6,9,13,19,28,...
+    //f(1) = 1
+    //f(2) = 2
+    //f(3) = 3
+    //f(50) = 178955183
+    System.out.println(sequenceRec(n));
+    
+  }
+}
